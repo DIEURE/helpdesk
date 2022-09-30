4f24052a69f6288152e2dc43234e032e799df7ce
DIEURE <dieure@gmail.com>
2022-09-30 16:23:53 -0300

    anotações JPA para classes de dominio

--- src/main/java/com/dlconecti/helpdesk/domain/Chamado.java
+++ src/main/java/com/dlconecti/helpdesk/domain/Chamado.java
@@ -4,20 +4,42 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 import com.dlconecti.helpdesk.domain.enums.Prioridade;
 import com.dlconecti.helpdesk.domain.enums.Status;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
+@Entity
 public class Chamado implements Serializable {
+	
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
+	@JsonFormat(pattern = "dd/mm/yyyy")
 	private LocalDate dataAbertura = LocalDate.now();
+	
+	@JsonFormat(pattern = "dd/mm/yyyy")
 	private LocalDate dataFechamento;
 	private Prioridade prioridade;
 	private Status status;
 	private String titulo;
 	private String observacoes;
+	
+	@ManyToOne
+	@JoinColumn(name= "tecnico_id")
 	private Tecnico tecnico;
+	
+	@ManyToOne
+	@JoinColumn(name= "cliente_id")
 	private Cliente cliente;
 
 	public Chamado(Integer id, Prioridade prioridade, Status status, String titulo, String observacoes, Tecnico tecnico,
